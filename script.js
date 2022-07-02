c15d19be75b482360910f29cf1131469b3bd73a6
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-07-01 19:53:40 -0500

    Bootstrap, jQuery, and buttons connected to JavaScript.

--- script.js
+++ script.js
@@ -14,3 +14,30 @@ function timer() {
 }
 
 timer();
+
+// var buttons = document.querySelectorAll("buttons");
+// var buttons = document.querySelectorAll("btnOne")
+
+// buttons.setAttribute("style", "font-size: 40px;");
+// buttons.setAttribute("style", "justify-content: center;");
+
+var btnOne = document.querySelector(".btnOne");
+var btnTwo = document.querySelector(".btnTwo");
+var btnThree = document.querySelector(".btnThree");
+var btnFour = document.querySelector(".btnFour");
+
+btnOne.addEventListener("click", function() {
+    console.log("Hello")
+});
+
+btnTwo.addEventListener("click", function() {
+    console.log("This is")
+});
+
+btnThree.addEventListener("click", function() {
+    console.log("is a button")
+});
+
+btnFour.addEventListener("click", function() {
+    console.log("Test!!!!")
+});
