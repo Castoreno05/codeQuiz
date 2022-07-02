8454dfc21fd86cb8172b4d63b326e54cc1fe9645
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-07-02 09:41:38 -0500

    Added a Play Game button and gave it the function to start the timer with click. Appended h2 tag to the page and removed the h2 tag in HTML.

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 var secondsLeft = 60;
 var timerEl = document.getElementById("timer");
+var startGame = document.querySelector("#startGame");
+// console.log(startGame);
+
+startGame.addEventListener("click", function() {
+    // console.log("click");
+    timer();
+});
 
 
 function timer() {
@@ -13,7 +20,15 @@ function timer() {
     }, 1000);
 }
 
-timer();
+// "Play Game" button and timer. ---------------------------------
+
+var body = document.body;
+var h2El = document.createElement("h2");
+var
+
+h2El.textContent = "This is where the first question will go.";
+
+body.appendChild(h2El);
 
 // var buttons = document.querySelectorAll("buttons");
 // var buttons = document.querySelectorAll("btnOne")
@@ -21,23 +36,26 @@ timer();
 // buttons.setAttribute("style", "font-size: 40px;");
 // buttons.setAttribute("style", "justify-content: center;");
 
-var btnOne = document.querySelector(".btnOne");
-var btnTwo = document.querySelector(".btnTwo");
-var btnThree = document.querySelector(".btnThree");
-var btnFour = document.querySelector(".btnFour");
+// var btnOne = document.querySelector(".btnOne");
+// var btnTwo = document.querySelector(".btnTwo");
+// var btnThree = document.querySelector(".btnThree");
+// var btnFour = document.querySelector(".btnFour");
 
-btnOne.addEventListener("click", function() {
-    console.log("Hello")
-});
+// btnOne.addEventListener("click", function() {
+//     console.log("Hello")
+// });
 
-btnTwo.addEventListener("click", function() {
-    console.log("This is")
-});
+// btnTwo.addEventListener("click", function() {
+//     console.log("This is")
+// });
 
-btnThree.addEventListener("click", function() {
-    console.log("is a button")
-});
+// btnThree.addEventListener("click", function() {
+//     console.log("a button")
+// });
 
-btnFour.addEventListener("click", function() {
-    console.log("Test!!!!")
-});
+// btnFour.addEventListener("click", function() {
+//     console.log("Test!!!!")
+// });
+
+// function answerOne() {
+// }
