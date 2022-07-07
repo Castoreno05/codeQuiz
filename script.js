2993ff9a02e15887d77b1c6e72d13180c479db3f
Matthew Castoreno <matthew.castoreno@yahoo.com>
2022-07-06 19:27:43 -0500

    Updates on the questions. Placed the questions and answers into objects. When a question is answered incorrectly it reduces the time by 10 seconds. When the answer is right you gain 20 points.

--- script.js
+++ script.js
@@ -1,4 +1,43 @@
+
+var questions = [
+
+    {
+        title: "Commonly used data types do NOT include:",
+        options: ["Quotes", "Parenthesis", "Curly Brackets", "Square Brackets"],
+        correctAnswer: "Alerts"
+    },
+
+    {
+        title: "The condition in an if/else statment is enclosed in ___.",
+        options: ["Quotes", "Parenthesis", "Curly Brackets", "Square Brackets"],
+        correctAnswer: "Parenthesis"
+    },
+
+    {
+        title: "String values must be closed within _____ when being assigned to a variable.",
+        options: ["Quotes", "Curly brackets", "Commas", "Parenthesis"],
+        correctAnswer: "Quotes"
+    },
+
+    {
+        titie: "Arrays in JavaScript can be used to store.",
+        options: ["Numbers and strings", "Other arrays", "Booleans", "All of the above"],
+        correctAnswer: "All of the above"
+    },
+
+    {
+        title: "A very useful tool used in development and debugging for printing content to the debugger is:",
+        options: ["JavaScript", "Terminal/Bash", "for loops",  "console.log"],
+        caorrectAnswer: "console.log"
+    }
+
+]
+
+// To track on which question user is
+
 // "Play Game" button and timer. ---------------------------------
+var totalScore = 0;
+var questionIndex = 0;
 var secondsLeft = 60;
 var timerEl = document.getElementById("timer");
 var startGame = document.querySelector("#startGame");
@@ -21,7 +60,38 @@ var btnFour = document.createElement("button");
 
 startGame.addEventListener("click", playGame);
 
+function displayQuestion() {
+    // 
+    h2El.textContent = questions[questionIndex].title;
+    btnOne.textContent = questions[questionIndex].options[0];
+    btnTwo.textContent = questions[questionIndex].options[1];
+    btnThree.textContent = questions[questionIndex].options[2];
+    btnFour.textContent = questions[questionIndex].options[3];
+
+}
+
+function handleClick(event) {
+    if(questions[questionIndex].correctAnswer === event.target.innerHTML) {
+        totalScore += 20;
+        // console.log(totalScore);
+    }
+    else {
+        // reduce the timer
+        secondsLeft -= 10;
+    }
+
+    if(questionIndex === questions.length - 1) {
+        resetGame();
+    }
+    else {
+        questionIndex++;
+        displayQuestion();
+    }
+}
+
 function playGame() {
+
+    displayQuestion()
 
     // console.log("click");
     // Text that is going inside the question "h2" and answers "li"
@@ -61,7 +131,10 @@ function playGame() {
     btnFour.setAttribute("style", "background-color:darkblue; border:none; color:black;");
 
     timer();
-    btnThree.addEventListener("click", buttonThree);
+    btnOne.addEventListener("click", handleClick);
+    btnTwo.addEventListener("click", handleClick);
+    btnThree.addEventListener("click", handleClick);
+    btnFour.addEventListener("click", handleClick);
 }
 
 // Reset function and eventListener
@@ -87,99 +160,10 @@ function timer() {
     }, 1000);
 }
 
-// Second Phase of questions ------------------------------------
-
-function buttonOne() {
-
-    // console.log("Helloooo Again!!");
-    // Text that is going inside the question "h2" and answers "li"
-    h2El.textContent = "The condition in an if/else statment is enclosed in ___.";
-    btnOne.textContent = "Quotes";
-    btnTwo.textContent = "Parenthesis";
-    btnThree.textContent = "Curly Brackets";
-    btnFour.textContent = "Square Brackets";
-
-    body.setAttribute("style", "background-color:violet;");
-    btnOne.setAttribute("style", "background-color:violet; border:none; color:black;");
-    btnTwo.setAttribute("style", "background-color:violet; border:none; color:black;");
-    btnThree.setAttribute("style", "background-color:violet; border:none; color:black;");
-    btnFour.setAttribute("style", "background-color:violet; border:none; color:black;");
-    
-    btnTwo.addEventListener("click", buttonTwo);
-
-}
-
-// Third Phase of questions ------------------------------------
-
-
-function buttonTwo() {
-
-    // console.log("Helloooo Again!!");
-    // Text that is going inside the question "h2" and answers "li"
-    h2El.textContent = "Arrays in JavaScript can be used to store.";
-    btnOne.textContent = "Numbers and strings";
-    btnTwo.textContent = "Other arrays";
-    btnThree.textContent = "Booleans";
-    btnFour.textContent = "All of the above";
-
-    body.setAttribute("style", "background-color:darkgrey;")
-    btnOne.setAttribute("style", "background-color:darkgrey; border:none; color:black;");
-    btnTwo.setAttribute("style", "background-color:darkgrey; border:none; color:black;");
-    btnThree.setAttribute("style", "background-color:darkgrey; border:none; color:black;");
-    btnFour.setAttribute("style", "background-color:darkgrey; border:none; color:black;");
-    
-    btnFour.addEventListener("click", buttonFour);
-
-}
-
-
-// Fourth Phase of questions ------------------------------------
-
-function buttonThree() {
-
-    // console.log("Helloooo Again!!");
-    // Text that is going inside the question "h2" and answers "li"
-    h2El.textContent = "String values must be closed within _____ when being assigned to a variable.";
-    btnOne.textContent = "Quotes";
-    btnTwo.textContent = "Curly brackets";
-    btnThree.textContent = "Commas";
-    btnFour.textContent = "Parenthesis";
-
-    body.setAttribute("style", "background-color:darkorange;")
-    btnOne.setAttribute("style", "background-color:darkorange; border:none; color:black;");
-    btnTwo.setAttribute("style", "background-color:darkorange; border:none; color:black;");
-    btnThree.setAttribute("style", "background-color:darkorange; border:none; color:black;");
-    btnFour.setAttribute("style", "background-color:darkorange; border:none; color:black;");
-    
-    btnOne.addEventListener("click", buttonOne);
-
-}
-
-
-// Fifth Phase of questions ------------------------------------
-
-function buttonFour() {
-
-    // console.log("Helloooo Again!!");
-    // Text that is going inside the question "h2" and answers "li"
-    h2El.textContent = "A very useful tool used in development and debugging for printing content to the debugger is:";
-    btnOne.textContent = "JavaScript";
-    btnTwo.textContent = "Terminal/Bash";
-    btnThree.textContent = "for loops";
-    btnFour.textContent = "console.log";
-
-    body.setAttribute("style", "background-color:pink;")
-    btnOne.setAttribute("style", "background-color:pink; border:none; color:black;");
-    btnTwo.setAttribute("style", "background-color:pink; border:none; color:black;");
-    btnThree.setAttribute("style", "background-color:pink; border:none; color:black;");
-    btnFour.setAttribute("style", "background-color:pink; border:none; color:black;");
-
-    // End Game
-
-}
-
-// myArray = [startGame, btnOne, btnTwo, btnThree, btnFour];
-
-
 // When inside the question. Find a way for only the correct selected answer to allow you to move forward.
 // Find a way to have the wrong answers prompt: "Incorrect"
+
+// body.setAttribute("style", "background-color:pink;")
+// body.setAttribute("style", "background-color:darkorange;")
+// body.setAttribute("style", "background-color:darkgrey;")
+// body.setAttribute("style", "background-color:violet;");
